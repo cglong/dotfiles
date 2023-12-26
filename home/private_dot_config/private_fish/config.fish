b16b468fbc3b0102a638e5e92f99ac71555632e7
Chris Long <chris@chrislong.dev>
2023-12-26 02:30:18 -0500

    Add script for Quick Look

--- home/private_dot_config/private_fish/config.fish
+++ home/private_dot_config/private_fish/config.fish
@@ -1,6 +1,8 @@
 set -gx EDITOR code
 set -gx HOMEBREW_NO_ENV_HINTS 1
 
+fish_add_path ~/bin
+
 if status is-interactive
     starship init fish | source
     source {$HOME}/.iterm2_shell_integration.fish
