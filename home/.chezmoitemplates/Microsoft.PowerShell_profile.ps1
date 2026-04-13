03b1e625d0a38db544e70d28fb9f43487f0b186e
Chris Long <chris@chrislong.dev>
2026-04-13 19:25:08 +0000

    Explicit PSReadLine only on Windows PowerShell

--- home/.chezmoitemplates/Microsoft.PowerShell_profile.ps1
+++ home/.chezmoitemplates/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,5 @@
 $Env:EDITOR = "code-insiders"
 
-Import-Module PSReadLine
-Set-PSReadLineOption -PredictionSource History
-
 New-Alias -Name "open" Invoke-Item
 New-Alias -Name "touch" New-Item
 New-Alias -Name "code" code-insiders
